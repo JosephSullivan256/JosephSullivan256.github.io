c29b2732da9f84fcc6614cb418c29208145b63a7
Joseph Sullivan <youknowjoejoe@gmail.com>
2018-07-28 16:02:56 -0700

    pre final

--- RoverRuckus/script.js
+++ RoverRuckus/script.js
@@ -107,13 +107,18 @@ rr = {
 		rr.ctx.fillRect(0,0,600,600);
 		
 		rr.ctx.fillStyle = 'black';
+		rr.ctx.beginPath();
+		rr.ctx.moveTo(rr.points[0].x,rr.points[0].y);
 		for(var i = 0; i < rr.points.length; i++){
+			rr.ctx.lineTo(rr.points[i].x,rr.points[i].y);
 			rr.ctx.fillRect(rr.points[i].x-5,rr.points[i].y-5,10,10);
 		}
+		//rr.ctx.closePath();
+		rr.ctx.stroke();
 	},
 	
 	mDown: function(e){
-		var pos = new Vec2(e.clientX,e.clientY);
+		var pos = new Vec2(rr.mx,rr.my);
 		var d = pos.minus(rr.points[0]).mag();
 		if(d < 20 && rr.points.length == 1){
 			rr.mouseDown = true;
@@ -125,12 +130,36 @@ rr = {
 	},
 	
 	mMove: function(e){
-		rr.mx = e.clientX;
-		rr.my = e.clientY;
+		var rect = canvas.getBoundingClientRect();
+		
+		rr.mx = e.x-rect.left;
+		rr.my = e.y-rect.top;
 	},
 	
 	start: function(){
+		var stringToSend = "";
+		for(var i = 0; i < rr.points.length; i++){
+			var pos = rr.points[i].minus(rr.points[0]).scaledBy(30.0/600.0);
+			stringToSend+=""+pos.x+","+pos.y;
+			if(i < rr.points.length-1) stringToSend +="|";
+		}
 		
+		var connection = new WebSocket('ws://159.65.105.241:4444');
+
+        connection.onopen = function () {
+            console.log('Connected!');
+            connection.send(stringToSend); // Send the message 'Ping' to the server
+        };
+
+        // Log errors
+        connection.onerror = function (error) {
+            console.log('WebSocket Error ' + error);
+        };
+
+        // Log messages from the server
+        connection.onmessage = function (e) {
+            console.log('Server: ' + e.data);
+        };
 	},
 	
 	reset: function(){
